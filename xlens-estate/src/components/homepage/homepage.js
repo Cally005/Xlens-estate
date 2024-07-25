4aa163e09d454a718170bc001db8801a8fb5016d
Cally005 <chibuikemorafu@gmail.com>
2024-07-25 01:52:09 +0100

    created all the other components

--- xlens-estate/src/components/homepage/homepage.js
+++ xlens-estate/src/components/homepage/homepage.js
@@ -1,168 +1,162 @@
-import React, { useState } from 'react'
-import  "./homepage.css"
-import dot1 from "../../assets/dot1.png"
-import logo from "../../assets/logo.png"
-import arrow from "../../assets/arrow.png"
-import arrowback from "../../assets/arrowback.png"
-import Projects from '../projects/projects'
-import bed from "../../assets/bed.png"
-import image2 from "../../assets/image2.png"
-import image3 from "../../assets/image3.png"
-
-
+import React, { useState } from 'react';
+import "./homepage.css";
+import dot1 from "../../assets/dot1.png";
+import logo from "../../assets/logo.png";
+import arrow from "../../assets/arrow.png";
+import arrowback from "../../assets/arrowback.png";
+import Projects from '../projects/projects';
+import bed from "../../assets/bed.png";
+import image2 from "../../assets/image2.png";
+import image3 from "../../assets/image3.png";
+import Services from '../services/service';
+import Footer from '../footer/footer';
 
 const images = [bed, image2, image3];
 
-const  Homepage = () => {
-
- const [currentImageIndex, setCurrentImageIndex] = useState(0);
+const Homepage = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   const nextImage = () => {
     setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
   const previousImage = () => {
-    setCurrentImageIndex((prevIndex) => 
+    setCurrentImageIndex((prevIndex) =>
       (prevIndex - 1 + images.length) % images.length
     );
   };
 
-
-
-
-
   return (
-
-<div className="homepage-container">
+    <div className="homepage-container">
       <div className="home-p" style={{ backgroundImage: `url(${images[currentImageIndex]})` }}>
-      <div class="film-cover">
-          <div className="header">
-                <div className='logo'>
-                  <img src={logo} alt='book' className="homepage-image"/>
-                </div>
-                <nav className='navbar'>
-                  <ul>
-                  <li><a href="#">Project </a></li>
-                  <li><a href="#">Services</a></li>
-                  <li><a href="#">About Us</a></li>
-                  <li><a href="#">Contact us</a></li>
-                  </ul>
-                </nav>
+        <div className="overlay"></div> {/* Overlay div */}
+        <div className="header">
+          <div className='logo'>
+            <img src={logo} alt='book' className="homepage-image" />
           </div>
-        {/* <img src={bed} alt='book' className="homepage-image"/> */}
+          <nav className='navbar'>
+            <ul>
+              <li><a href="#">Project</a></li>
+              <li><a href="#">Services</a></li>
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Contact us</a></li>
+            </ul>
+          </nav>
+        </div>
 
-        <div>
-            <div className='p-pox'>
+        <div className='parent'>
+          {currentImageIndex === 0 && (
+            <div className='slide-1'>
               <hr />
-              <div className='slide-content'>
-                {currentImageIndex === 0 && (
-                  <div className='slide-1'>
-                    <h1>01</h1>
-                    <div className='s-content'>
-                      <h2>GLATT Estate</h2>
-                            <p>It is a long established fact that a reader
-                         will be distracted by the readable content<br></br>
-                         of a page when looking at its layout. 
-                         The point of using Lorem<br></br> ipsum.</p>
-                    </div>
-                  </div>
-                )}
+              <h1>01</h1>
+              <h2>GLATT Estate</h2>
+              <p>
+                It is a long established fact that a reader
+                will be distracted by the readable content
+                of a page when looking at its layout.
+                The point of using Lorem ipsum.
+              </p>
+            </div>
+          )}
 
-                {currentImageIndex === 1 && (
-                  <div className='slide-2'>
-                    <h1>02</h1>
-                    <div className='s-content'>
-                      <h2>MHQ Project</h2>
-                      <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem ipsum.</p>
-                    </div>
-                  </div>
-                )}
-
-                {currentImageIndex === 2 && (
-                  <div className='slide-3'>
-                    <h1>03</h1>
-                    <div className='s-content'>
-                      <h2>Blum</h2>
-                      <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem ipsum.</p>
-                    </div>
-                  </div>
-                )}
-
-                <div className='dots'>
-                  <h4>+ SEE PROJECT</h4>
-                  <ul>
-                    <li className={`dot-1 ${currentImageIndex === 0 ? 'active' : ''}`}></li>
-                    <li className={`dot-2 ${currentImageIndex === 1 ? 'active' : ''}`}></li>
-                    <li className={`dot-3 ${currentImageIndex === 2 ? 'active' : ''}`}></li>
-                  </ul>
-                </div>
+          {currentImageIndex === 1 && (
+            <div className='slide-2'>
+              <hr />
+              <h1>02</h1>
+              <div className='s-content'>
+                <h2>MHQ Project</h2>
+                <p>
+                  It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem ipsum.
+                </p>
               </div>
             </div>
-          </div>
-        
+          )}
 
+          {currentImageIndex === 2 && (
+            <div className='slide-3'>
+              <hr />
+              <h1>03</h1>
+              <div className='s-content'>
+                <h2>Blum</h2>
+                <p>
+                  It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem ipsum.
+                </p>
+              </div>
+            </div>
+          )}
+
+          <div className='dots'>
+            <h4>+ SEE PROJECT</h4>
+            <div className='dots-con'>
+            <ul>
+              <li className={`dot-1 ${currentImageIndex === 0 ? 'active' : ''}`}></li>
+              <li className={`dot-2 ${currentImageIndex === 1 ? 'active' : ''}`}></li>
+              <li className={`dot-3 ${currentImageIndex === 2 ? 'active' : ''}`}></li>
+            </ul>
+            </div>
+          </div>
+        </div>
 
         <div className='b-nav'>
-
           <div>
-              <p>Latest Project</p>
-              <h4>MOdern industrial Loft</h4>
-              <p>Year completed 2022</p>
+            <p>Latest Project</p>
+            <h4>Modern Industrial Loft</h4>
+            <p>Year completed 2022</p>
           </div>
           <div>
-              <p>Location</p>
-     
-              <h4>New York, USA</h4>
+            <p>Location</p>
+            <h4>New York, USA</h4>
           </div>
-          <div >
-              <h4>Theme & style</h4>
+          <div>
+            <h4>Theme & style</h4>
           </div>
           <div className='arrow'>
-                <ul>
-                    <li> <img src={arrow} alt='book' className="arrow-1" onClick={nextImage}/></li>
-                    <li> <img src={arrowback} alt='book' className="arrow-2" onClick={previousImage}/></li>
-                </ul>  
-            </div>
-
-            {/* Projects  section*/}
-            <Projects/>
-
+            <ul>
+              <li><img src={arrow} alt='book' className="arrow-1" onClick={nextImage} /></li>
+              <li><img src={arrowback} alt='book' className="arrow-2" onClick={previousImage} /></li>
+            </ul>
+          </div>
         </div>
-        </div>
+      </div>
+      <Projects/>
+      <Services/>
+      <Footer/>
     </div>
-</div>
  
-  
-
-
- 
-
-        
-   
-  
-   
-
-    
-   
-  )
-
+  );
 }
-export default Homepage
+
+export default Homepage;
 
 
-
-
-// import React from 'react'
+// import React, { useState } from 'react'
 // import  "./homepage.css"
-// import bed from "../../assets/bed.png"
 // import dot1 from "../../assets/dot1.png"
 // import logo from "../../assets/logo.png"
 // import arrow from "../../assets/arrow.png"
 // import arrowback from "../../assets/arrowback.png"
+// import Projects from '../projects/projects'
+// import bed from "../../assets/bed.png"
+// import image2 from "../../assets/image2.png"
+// import image3 from "../../assets/image3.png"
+// import Services from '../services/service'
+// import Footer from '../footer/footer'
 
+
+// const images = [bed, image2, image3];
 // const  Homepage = () => {
 
+//   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
+//     const nextImage = () => {
+//       setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+//     };
+  
+//     const previousImage = () => {
+//       setCurrentImageIndex((prevIndex) => 
+//         (prevIndex - 1 + images.length) % images.length
+//       );
+//     };
 
 
 
@@ -172,22 +166,20 @@ export default Homepage
 
 //   return (
 
-//      <div className="homepage-container">
-//       <div className="homepage-content">
-//         <div>
-//           <img src={logo} alt='book' className="homepage-image"/>
-//         </div>
-//         <nav className='navbar'>
-//           <ul>
-//           <li><a href="#">Project </a></li>
-//           <li><a href="#">Services</a></li>
-//           <li><a href="#">About Us</a></li>
-//           <li><a href="#">Contact us</a></li>
-//           </ul>
-//         </nav>
-//       </div>
-//         <img src={bed} alt='book' className="homepage-image"/>
-
+//     <div className="homepage-container">
+//            <div className="home-p" style={{ backgroundImage: `url(${images[currentImageIndex]})` }}>
+//                <div className="header">
+//                      <div className='logo'>
+//                        <img src={logo} alt='book' className="homepage-image"/>
+//                      </div>
+//                      <nav className='navbar'>
+//                           <ul>
+//                           <li><a href="#">Project </a></li>
+//                           <li><a href="#">Services</a></li>
+//                           <li><a href="#">About Us</a></li>
+//                           <li><a href="#">Contact us</a></li>
+//                           </ul>
+//                      </nav>
 //     <div>
 //         <div className='p-pox'>
 //             <hr></hr>
@@ -195,7 +187,7 @@ export default Homepage
 //                <h1>o1</h1>
 //             <div>
 //               <h2>GLATT Esate</h2>
-//               <p>It is a long establihed fact taht a reader will be distracted by the readable <br> content of a page when looin ar uts layout. the point of using Lorem ipsum  </br></p>
+//               <p>It is a long establihed fact taht a reader will be distracted by the readable content of a page when looin ar uts layout. the point of using Lorem ipsum  </p>
 //             </div>
 //             <br></br>
 //                <h4>+ SEE PROJECT</h4>
@@ -219,7 +211,7 @@ export default Homepage
 //           </div>
 //           <div>
 //               <p>Location</p>
-//               <br></br>
+          
 //               <h4>New York, USA</h4>
 //           </div>
 //           <div>
@@ -237,6 +229,8 @@ export default Homepage
 //         </div>
 
 //     </div>
+//  </div>
+//  </div>
 //  </div>
         
    
---
+++ xlens-estate/src/components/services/service.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import "./service.css"
+import loouge4 from "../../assets/loouge4.png"
+import lounge from "../../assets/lounge.png"
+import Lounge3 from "../../assets/Lounge3.png"
+import bed from "../../assets/bed.png";
+import image2 from "../../assets/image2.png";
+import image3 from "../../assets/image3.png";
+
+const Service = () => {
+    const [clickedIndex, setClickedIndex] = useState(null);
+    const [currentImages, setCurrentImages] = useState([loouge4, lounge, Lounge3]);
+  
+    const imageSets = {
+      Interior: [ Lounge3, Lounge3, lounge ],
+      Exterior: [lounge, image3,  Lounge3],
+      Visualization: [image2, bed,  image3],
+      'Floor Plans': [ Lounge3, image2, bed],
+    };
+  
+    const handleClick = (index) => {
+      setClickedIndex(index);
+      setCurrentImages(imageSets[Object.keys(imageSets)[index]]);
+    };
+  
+  return (
+    <div className='contianer'>
+        <div className='head'>
+        <h1>service</h1>
+        </div>
+        <nav  className='nav-a'>
+        <ul>
+          {['Interior', 'Exterior', 'Visualization', 'Floor Plans'].map((item, index) => (
+            <li
+              key={index}
+              onClick={() => handleClick(index)}
+              className={clickedIndex === index ? 'active' : ''}
+            >
+              <h3>{item}</h3>
+              <hr className={clickedIndex === index ? 'active' : ''} />
+            </li>
+          ))}
+        </ul>
+        </nav>
+        <div className='red'>
+            <h5>Rendering pricing estimate for interior design</h5>   
+        </div>
+        <nav  className='nav-b'>
+            <ul>
+                <li><h3>Timeframe<span>: 3-6 days</span></h3></li>
+                
+                <li><h3>Price Range<span>: $450-$950</span></h3></li>
+                
+                <li><h3>Contact Us</h3></li>
+               
+            </ul>
+        </nav>
+        <div className='lounge'>
+            
+        {currentImages.map((image, index) => (
+          <img src={image} alt={`book-${index}`} className="lounge-image" key={index} />
+        ))}
+        </div>
+        
+        </div>
+  )
+}
+
+export default Service
