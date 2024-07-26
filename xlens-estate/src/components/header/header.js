cc79d524e5999b8452dc173006cebc708eff68ee
Cally005 <chibuikemorafu@gmail.com>
2024-07-27 00:22:15 +0100

    added the hanburger on small screens

--- xlens-estate/src/components/header/header.js
+++ xlens-estate/src/components/header/header.js
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './header.css';
 import logo from '../../assets/logo.png';
+import { FaBars } from 'react-icons/fa'; // Use any icon library you prefer
 
 const Header = ({ isTransparent }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
   return (
-    <header className={`header ${isTransparent ? 'transparent' : 'black'}`}>
+    <header className={`header ${isTransparent ? 'transparent' : 'black'} ${isSidebarOpen ? 'open' : ''}`}>
       <div className="logo">
-      <Link to="/"> <img src={logo} alt="logo" /></Link>
+        <Link to="/"><img src={logo} alt="logo"  className={`navbar ${isSidebarOpen ? 'active' : ''}`} /></Link>
       </div>
-      <nav className="navbar">
+      <nav className={`navbar ${isSidebarOpen ? 'active' : ''}`}>
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/projects">Projects</Link></li>
-          <li><Link to="/services">Services</Link></li>
-          <li><Link to="/about">About Us</Link></li>
-          <li><Link to="/contact">Contact Us</Link></li>
+        <li onClick={closeSidebar}><Link to="/">Home</Link></li>
+          <li onClick={closeSidebar}><Link to="/projects">Projects</Link></li>
+          <li onClick={closeSidebar}><Link to="/services">Services</Link></li>
+          <li onClick={closeSidebar}><Link to="/about">About Us</Link></li>
+          <li onClick={closeSidebar}><Link to="/contact">Contact Us</Link></li>
         </ul>
       </nav>
+      <div className="menu-icon" onClick={toggleSidebar}>
+        <FaBars />
+      </div>
     </header>
   );
 };
 
 export default Header;
+
